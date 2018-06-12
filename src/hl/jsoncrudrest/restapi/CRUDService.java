6ce03317a50ca9fa90d6204b1a2525572f34fa7a
onghuilam <onghuilam@gmail.com>
2018-06-12 11:51:18 +0800

    increase version

--- src/hl/jsoncrudrest/restapi/CRUDService.java
+++ src/hl/jsoncrudrest/restapi/CRUDService.java
@@ -54,7 +54,7 @@ public class CRUDService extends HttpServlet {
 	protected static String _PAGINATION_RESULT_SECTION 	= JsonCrudConfig._LIST_RESULT;
 	protected static String _PAGINATION_META_SECTION 	= JsonCrudConfig._LIST_META;	
 	
-	private static String _VERSION = "0.3.8 beta";
+	private static String _VERSION = "0.3.9 beta";
 		
 	private Map<Integer, Map<String, String>> mapAutoUrlCrudkey 	= null;
 	private Map<Integer, Map<String, String>> mapMappedUrlCrudkey 	= null;
