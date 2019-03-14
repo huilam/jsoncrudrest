e3cba795453bb3a7118f9972c48eec31ab996a82
onghuilam <onghuilam@gmail.com>
2019-03-14 13:06:04 +0800

    update sample test proxy example

--- src/hl/jsoncrudrest/plugins/SampleTestProxy.java
+++ src/hl/jsoncrudrest/plugins/SampleTestProxy.java
@@ -29,6 +29,14 @@ public class SampleTestProxy implements ICRUDServicePlugin {
 
 	public HttpResp postProcess(CRUDServiceReq aCrudReq, HttpResp aHttpResp) {
 		
+		System.out.println();
+		System.out.println("[ "+this.getClass().getSimpleName()+".postProcess ]");
+		System.out.println("httpStatus="+aHttpResp.getHttp_status());
+		System.out.println("httpStatusMsg="+aHttpResp.getHttp_status_message());
+		System.out.println("contentType="+aHttpResp.getContent_type());
+		System.out.println("contentData="+aHttpResp.getContent_data());
+		System.out.println();
+		
 		return aHttpResp;
 	}
 	
