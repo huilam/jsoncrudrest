3886a6f806d6045139a23de461eac9188ad8f05a
onghuilam <onghuilam@gmail.com>
2021-06-15 13:34:30 +0800

    remove unused import

--- src/hl/jsoncrudrest/restapi/CRUDServiceUtil.java
+++ src/hl/jsoncrudrest/restapi/CRUDServiceUtil.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.StringTokenizer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.regex.Matcher;
